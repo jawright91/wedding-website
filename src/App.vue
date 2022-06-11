b819798ecaecf088fffb563901566557b8229341
Joshua Wright <jawright91@gmail.com>
2022-06-11 14:13:06 -0500

    removed remote links and added empty vues

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
+  <div class="page-content page-home">
+    <!-- home logo -->
+    <div class="row">
+      <div class="home-logo col-sm-8 col-sm-offset-2">
+        <p class="couple-title">Josh & Nguhi</p>
+      </div>
     </div>
-    <router-view />
+    <!-- /home-logo -->
+    <!-- Countdown container -->
+    <div class="row">
+      <div class="countdown-container">
+        <!-- icon -->
+        <div class="countdown-icon">
+          <img src="img/ico-heart2.png" alt="" />
+        </div>
+        <!-- /icon -->
+        <!-- countdown title top -->
+        <h2 class="countdown-title countdown-title1">
+          <span class="countdown-title-borders">October 15, 2022 in Midlothian, TX</span>
+        </h2>
+        <!-- /countdown title top -->
+        <!-- countdown ( Edit it on main.js )-->
+        <div class="row">
+          <div class="col-xs-8 col-xs-offset-2">
+            <div id="countdown" class="row"></div>
+          </div>
+        </div>
+        <!-- /countdown -->
+        <!-- countdown title bot -->
+        <h3 class="countdown-title countdown-title2">
+          <span class="countdown-title-borders">Until we get Married!</span>
+        </h3>
+        <!-- countdown title /bot -->
+      </div>
+    </div>
+    <!-- /Countdown Container -->
   </div>
 </template>
 
