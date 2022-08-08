b292ce93e48eb72c375e31635627a9a4d5e62c47
Joshua Wright <jawright91@gmail.com>
2022-08-07 22:23:28 -0500

    effin css, man

--- src/views/RSVP.vue
+++ src/views/RSVP.vue
@@ -4,14 +4,10 @@
       <!-- mai title -->
       <h2 class="title1 couple-title1 title1-border">R.S.V.P.</h2>
       <!-- /main title -->
-      <div class="page-intro">
-        <p>
-          As we get closer to the event, when we send formal invitations, you will be able to use this site to RSVP to
-          let us know you will be attending. The invitation will contain instructions on how to RSVP.
-        </p>
+
+      <div class="page-intro" style="z-index: 9999999999999">
+        <script type="application/javascript" src="https://joshandnguhiwedding.rsvpify.com/embed"></script>
       </div>
-      <script type="application/javascript" src="https://joshandnguhiwedding.rsvpify.com/embed"></script>
-      <!--<iframe src="https://joshandnguhiwedding.rsvpify.com" height="500px" witdh="600px"></iframe>-->
     </div>
   </div>
 </template>
